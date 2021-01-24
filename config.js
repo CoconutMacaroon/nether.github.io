81db4f03f6ae17847b02dcf9f891dd815cfb31ff
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2021-01-23 19:35:32 -0800

    Add files

---
+++ config.js
@@ -0,0 +1,55 @@
+var CONFIG = {
+  "maps": {
+    "data": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1611455799,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "Nether",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "data_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "data",
+      "worldName": "data",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "data"
+  ],
+  "tileSetGroups": {
+    "data_isometric_t1": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
